02cc3a4b6e7df9ca2ceecb41f664a2c9fe7d5cdf
Marcin Hoppe <marcin.hoppe@particular.net>
2017-11-18 21:18:17 +0100

    Projekt startowy do ćwiczenia 05

---
+++ 05-api-links/PasswordManager/PasswordManager/Controllers/WebsiteController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordManager.Model;
+
+namespace PasswordManager.Controllers
+{
+    public class WebsiteController : Controller
+    {
+        private readonly WebsiteContext context;
+
+        public WebsiteController(WebsiteContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost("/websites")]
+        public IActionResult RegisterWebsite([FromBody] ViewModel.Website website)
+        {
+            context.Websites.Add(new Website
+            {
+                Url = website.Url
+            });
+            context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ 05-api-links/PasswordManager/PasswordManager/Model/Website.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Security.AccessControl;
+
+namespace PasswordManager.Model
+{
+    public class Website
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; }
+        public Credentials Credentials { get; set; }
+    }
+}
