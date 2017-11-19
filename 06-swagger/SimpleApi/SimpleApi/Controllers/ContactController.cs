06e8d6d7d535d4b822a87159638219c100d33bfc
Marcin Hoppe <marcin.hoppe@particular.net>
2017-11-19 15:40:04 +0100

    Projekt startowy do ćwiczenia 06

---
+++ 06-swagger/SimpleApi/SimpleApi/Controllers/ContactController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SimpleApi.Model;
+
+namespace SimpleApi.Controllers
+{
+    [Route("contacts")]
+    public class ContactController : Controller
+    {
+        private static readonly List<ContactInfo> contacts = new List<ContactInfo>()
+        {
+            new ContactInfo
+            {
+                FirstName = "Marcin",
+                LastName = "Hoppe",
+                Email = "marcin.hoppe@acm.org",
+                PhoneNumber = "+48 123 456 789"
+            },
+            new ContactInfo
+            {
+                FirstName = "Jan",
+                LastName = "Kowalski",
+                Email = "jan.kowalski@gmail.com",
+                PhoneNumber = "+48 987 654 321"
+            }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(contacts);
+        }
+
+        [HttpGet("{email}")]
+        public IActionResult Get(string email)
+        {
+            var contactInfo = contacts.FirstOrDefault(c => c.Email == email);
+            if (contactInfo == null)
+            {
+                return NotFound();
+            }
+            return Json(contactInfo);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] ContactInfo contactInfo)
+        {
+            contacts.Add(contactInfo);
+            return Ok();
+        }
+
+        [HttpDelete("{email}")]
+        public IActionResult Delete(string email)
+        {
+            var contactInfo = contacts.FirstOrDefault(c => c.Email == email);
+            if (contactInfo == null)
+            {
+                return NotFound();
+            }
+            contacts.Remove(contactInfo);
+            return Ok();
+        }
+    }
+}
---
+++ 06-swagger/SimpleApi/SimpleApi/Model/ContactInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleApi.Model
+{
+    public class ContactInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
