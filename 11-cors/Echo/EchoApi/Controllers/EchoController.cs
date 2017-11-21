a13cb049dc7af13f6f8fa123ad424faa796ac0c7
Marcin Hoppe <marcin.hoppe@particular.net>
2017-11-21 23:13:48 +0100

    Projekt startowy do ćwiczenia 11

---
+++ 11-cors/Echo/EchoApi/Controllers/EchoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EchoApi.Controllers
+{
+    [Route("echo")]
+    public class EchoController : Controller
+    {
+        [HttpGet("{message}")]
+        public IActionResult Echo(string message)
+        {
+            return Content(message);
+        }
+    }
+}
